abc15d3c1850ceca232cd9c6f620875e825f90f1
Phuoc Bui <phuocbui90@gmail.com>
2025-07-11 10:33:53 -0500

    added locators to landingpage

---
+++ pages/LandingPage.ts
@@ -0,0 +1,32 @@
+import { Locator, Page } from "@playwright/test";
+
+export class LandingPage {
+  loginButton: Locator;
+  hudlLogin: Locator;
+  wyscoutLogin: Locator;
+  volleymetricsLogin: Locator;
+  signalLogin: Locator;
+  instatBasketballLogin: Locator;
+  instatHockeyLogin: Locator;
+  iqforAmericanFootballLogin: Locator;
+  statsbombLogin: Locator;
+  balltimeLogin: Locator;
+  titanLogin: Locator;
+
+  constructor(page: Page) {
+    this.loginButton = page.getByTestId("login-select");
+    this.hudlLogin = page.getByTestId("login-hudl");
+    this.wyscoutLogin = page.getByTestId("login-wyscout");
+    this.volleymetricsLogin = page.getByTestId("login-volleymetrics");
+    this.signalLogin = page.getByTestId("login-signal");
+    this.instatBasketballLogin = page.getByTestId("login-instatbasketball");
+    this.instatHockeyLogin = page.getByTestId("login-instathockey");
+    this.iqforAmericanFootballLogin = page.getByTestId("login-iq");
+    this.statsbombLogin = page.getByTestId("login-statsbomb");
+    this.balltimeLogin = page.getByTestId("login-balltime");
+    this.titanLogin = page.getByTestId("login-titan");
+  }
+
+
+  
+}
