bf97425fe657fd1ce1a0c3d074258a5947fef678
Phuoc Bui <phuocbui90@gmail.com>
2025-07-12 15:30:32 -0500

    Add two more tests for invalid credentials

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -2,10 +2,39 @@ import { expect } from "@playwright/test";
 import { test } from "../fixtures/fixtures";
 
 test.describe("Navigation", () => {
-  test("Navigate to the homepage successfully", async ({ homePage }) => {
-    await homePage.navigate();
-
-    const currentUrl = await homePage.getCurrentUrl();
+  test("Navigate to the landing page successfully", async ({ landingPage }) => {
+    const currentUrl = await landingPage.getCurrentUrl();
     expect(currentUrl).toEqual("https://www.hudl.com/");
+  });
+
+  test("Clicking on Hudl Link navigates to Hudl login", async ({ landingPage }) => {
+    await landingPage.openHudlLoginPage();
+    const currentUrl = await landingPage.getCurrentUrl();
+    expect(currentUrl).toContain("identity.hudl.com");
+  });
+});
+
+test.describe("Login", () => {
+  test("User able to successfully log into Hudl with valid credentials", async ({ hudlLoginPage, page }) => {
+    const { EMAIL, PASSWORD } = process.env;
+    await hudlLoginPage.loginViaEmail(EMAIL, PASSWORD);
+
+    expect(page.url()).toContain("/home");
+  });
+
+  test("User unable to login with invalid email", async ({ hudlLoginPage, page }) => {
+    await hudlLoginPage.enterInEmail("badEmail");
+
+    expect(hudlLoginPage.invalidEmailError).toBeVisible();
+    expect(hudlLoginPage.invalidEmailError).toContainText("Enter a valid email.");
+  });
+
+  test("User unable to login with invalid password", async ({ hudlLoginPage, page }) => {
+    const { EMAIL } = process.env;
+    await hudlLoginPage.enterInEmail(EMAIL);
+    await hudlLoginPage.enterInPassword("badPassword");
+
+    expect(hudlLoginPage.incorrectPasswordError).toBeVisible();
+    expect(hudlLoginPage.incorrectPasswordError).toContainText("Your email or password is incorrect. Try again.");
   });
 });
