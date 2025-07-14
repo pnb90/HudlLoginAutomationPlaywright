5568371cdc9475914f9728c538166e567ba8724d
Phuoc Bui <phuocbui90@gmail.com>
2025-07-14 12:10:44 -0500

    Update navigate method and add route to basepage

--- pages/BasePage.ts
+++ pages/BasePage.ts
@@ -3,21 +3,14 @@ import { Page } from "@playwright/test";
 export abstract class BasePage {
   page: Page;
   baseUrl: string = "https://www.hudl.com";
+  route: string = "";
 
   constructor(page: Page) {
     this.page = page;
   }
 
-  async navigate(url?: string, route?: string): Promise<void> {
-    let navigationUrl: string = this.baseUrl;
-
-    if (route) {
-      navigationUrl = this.baseUrl + route;
-    } else if (url) {
-      navigationUrl = url;
-    }
-
-    await this.page.goto(navigationUrl);
+  async navigate(): Promise<void> {
+    await this.page.goto(`${this.baseUrl}${this.route}`);
   }
 
   getCurrentUrl(): string {
