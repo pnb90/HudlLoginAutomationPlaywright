f830bc4f7d3cd076b7510504206e6e69fc8f3f57
Phuoc Bui <phuocbui90@gmail.com>
2025-07-14 09:34:23 -0500

    oops forgot to extend HudlHomePage as a BasePage

--- pages/HudlHomePage.ts
+++ pages/HudlHomePage.ts
@@ -1,9 +1,12 @@
 import { Locator, Page } from "@playwright/test";
+import { BasePage } from "./BasePage";
 
-export class HudlHomePage {
+export class HudlHomePage extends BasePage {
   globalNavBar: Locator;
+  route = "/home";
 
   constructor(page: Page) {
+    super(page);
     // this test id has a typo
     this.globalNavBar = page.getByTestId("gloabl-navbar");
   }
